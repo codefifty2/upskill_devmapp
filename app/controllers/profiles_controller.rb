4e5ded1a54e97f4fb04da0792a7c5ffede2e136d
codefifty2 <willesver@gmail.com>
2018-05-19 06:06:47 +0000

    Added users profile form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,7 @@
-class ProfilesController < ApplicationController
-  # Get to /user/:user_id/profile/new
-  def new
-    # Render blank profile details form
+  class ProfilesController < ApplicationController
+    # Get to /user/:user_id/profile/new
+    def new
+      # Render blank profile details form
+      @profile = Profile.new
+    end
   end
-end
